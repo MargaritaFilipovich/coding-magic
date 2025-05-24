836e634adfa212dd7bf1201c9b6e2a64320fac04
kirill-kovalchukk <153985133+kirill-kovalchukk@users.noreply.github.com>
2025-05-24 11:18:18 +0300

    hello

--- src/js/section-num.js
+++ src/js/section-num.js
@@ -1,9 +1,36 @@
-function findMax() {
-    const n1 = Number(document.getElementById('num1').value);
-    const n2 = Number(document.getElementById('num2').value);
-    const n3 = Number(document.getElementById('num3').value);
-    const max = Math.max(n1, n2, n3);
-    document.getElementById('result').innerText = `Найбільше число, яке ви ввели - ${max}`;
-  }
+document.addEventListener("DOMContentLoaded", function () {
+  const num1 = document.getElementById("num1");
+  const num2 = document.getElementById("num2");
+  const num3 = document.getElementById("num3");
+  const resultDiv = document.getElementById("max-result");
 
-  document.getElementById('button-num').addEventListener('click', findMax);
+  num1.addEventListener("input", () => {
+    if (num1.value) {
+      setTimeout(() => {
+        validateField(num2);
+        setTimeout(() => {
+          validateField(num3);
+        }, 100);
+      }, 0);
+    }
+    checkAndShowMax();
+  });
+
+  [num2, num3].forEach(input => {
+    input.addEventListener("input", () => {
+      input.setCustomValidity("");
+      checkAndShowMax();
+    });
+  });
+
+  num1.addEventListener("input", checkAndShowMax);
+
+  function checkAndShowMax() {
+    if (num1.value && num2.value && num3.value) {
+      const max = Math.max(+num1.value, +num2.value, +num3.value);
+      resultDiv.textContent = `Найбільше число яке ви ввели: ${max}`;
+    } else {
+      resultDiv.textContent = "";
+    }
+  }
+});
