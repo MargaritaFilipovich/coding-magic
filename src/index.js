347afecb5afe0b8ae373c90a0d68e0bc9e726e89
MargaritaFilipovich <153985075+MargaritaFilipovich@users.noreply.github.com>
2025-05-25 10:16:43 +0300

    gdgd

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import './js/section-1.js';
 import './js/section-2.js';
 import './js/section-num.js';
 import './js/calculator.js';
+import './js/dino.js'
 import './js/sec.js';
 import './js/hh.js';
 import './js/scientists.js';
